e4ac725c96a74bf9ba6eadecbd5294928b505e45
cph <wl02026336@gmail.com>
2024-08-18 09:43:43 +0800

    [feat] 初始化專案
    因應需求調整:
    1. spring boot initial
    2. spring boot lib 載入 jpa、quartz、web、aop、swagger 相關 lib
    3. spring boot properties 設定 port、h2 相關資訊、日期格式

---
+++ src/main/java/com/cathay/bank/coindeskhelper/CoindeskhelperApplication.java
@@ -0,0 +1,13 @@
+package com.cathay.bank.coindeskhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoindeskhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoindeskhelperApplication.class, args);
+	}
+
+}
